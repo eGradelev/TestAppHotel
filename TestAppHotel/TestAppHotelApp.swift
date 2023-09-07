9a3c66d6685c642b56c7d257d8116c5effe9aecd
Elisey <gradelevelisey@mail.ru>
2023-09-07 20:14:25 +0300

    Initial Commit

---
+++ TestAppHotel/TestAppHotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppHotelApp.swift
+//  TestAppHotel
+//
+//  Created by Gradelev Elisey on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAppHotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
